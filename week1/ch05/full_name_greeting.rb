2bf47f771521def50af1b28cb81eeb98fdc20ffa
kevrubio4 <mathwizkevin@gmail.com>
2018-06-19 22:40:30 -0700

    All Exercises Completed Plus Lab

--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -5,4 +5,4 @@ middle_name = gets.chomp
 puts 'What is your last name?'
 last_name = gets.chomp
 puts 'Greetings ' + first_name + ' ' + middle_name +  ' ' + last_name + '!'
-puts 'That\'s a long name!'
+puts 'That\'s a very nice name!'
